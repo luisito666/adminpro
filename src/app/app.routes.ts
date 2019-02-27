17e7dd3361e4ce923667ad741a50c1a036fb776a
luisito666 <luispenagos91@gmail.com>
2019-02-27 16:58:10 -0500

    Inicializando el amdin pro

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
+
+
+const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: NopagefoundComponent }
+];
+
+export const APP_ROUTES = RouterModule.forRoot(routes, {useHash: true});
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+
+// Modules
+import { SharedModule } from '../shared/shared.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { PAGES_ROUTES } from './pages.routes';
+
+
+@NgModule({
+    declarations: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    imports: [ 
+        SharedModule,
+        PAGES_ROUTES
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    providers: [],
+})
+export class PagesModule {}
